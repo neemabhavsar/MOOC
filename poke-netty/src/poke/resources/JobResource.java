552eaed50f49e68ed03e1193675d2f4276334089
gayathriSrinivas <gaya.0408@gmail.com>
2014-04-05 21:26:31 -0700

    Client Files

--- poke-netty/src/poke/resources/JobResource.java
+++ poke-netty/src/poke/resources/JobResource.java
@@ -20,43 +20,223 @@ import org.slf4j.LoggerFactory;
 
 import poke.server.resources.Resource;
 import poke.server.resources.ResourceUtil;
+import eye.Comm.JobOperation;
+import eye.Comm.JobOperation.JobAction;
+import eye.Comm.NameValueSet;
+import eye.Comm.JobDesc.JobCode;
+import eye.Comm.NameValueSet.NodeType;
+import eye.Comm.JobDesc;
 import eye.Comm.Payload;
 import eye.Comm.PokeStatus;
 import eye.Comm.Request;
 import eye.Comm.SignUp;
 
 public class JobResource implements Resource {
+	
 	protected static Logger logger = LoggerFactory.getLogger("server");
+	private Request.Builder rb;
+	
 
 	@Override
 	public Request process(Request request) {
-		// TODO Auto-generated method stub
+
 		// TODO add code to process the message/event received
-		logger.info("Welcome: " + request.getBody().getSignUp().getFname()
-				+ " " + request.getBody().getSignUp().getLname());
-		// logger.info("poke: " + request.getBody().getPing().getTag());
+		logger.info("Outside Request for ListCourses ::" + request.getBody().getJobOp().getData().getNameSpace()
+				+ " :: " +  request.getBody().getJobOp().getData().getJobId());
+		
+		if(request.getBody().getJobOp().getData().getNameSpace().equals("listcourses")){
+			logger.info("Request for ListCourses ::" + request.getBody().getJobOp().getData().getOptions().getName()
+					+ " :: " +  request.getBody().getJobOp().getData().getOptions().getValue());
+			
+			//DB Call to Fetch
+			
+			
+			
+			
+			/*
+			SignUp.Builder sb = SignUp.newBuilder();
+			sb.setEmail(request.getBody().getSignUp().getEmail());
+			sb.setPassword(request.getBody().getSignUp().getPassword());
+			sb.setFname(request.getBody().getSignUp().getFname());
+			sb.setLname(request.getBody().getSignUp().getLname());
+			pb.setSignUp(sb.build());
+			*/
+			
+			// payload
+			
+			Payload.Builder pb = Payload.newBuilder();
+			
+			NameValueSet.Builder nb1 = NameValueSet.newBuilder();
+			nb1.setName("coursename");
+			nb1.setValue("cmpe277");
+			nb1.setNodeType(NodeType.VALUE);
+			
+			
+			NameValueSet.Builder nb2 = NameValueSet.newBuilder();
+			nb2.setName("coursename");
+			nb2.setValue("cmpe275");
+			nb2.setNodeType(NodeType.VALUE);
+			
+			
+			NameValueSet.Builder main = NameValueSet.newBuilder();
+			main.setNodeType(NodeType.NODE);
+			main.addNode(0, nb1.build());
+			main.addNode(1, nb2.build());
+			
+			
+			
+			JobDesc.Builder jb = JobDesc.newBuilder();
+			jb.setNameSpace("listcourses");
+			jb.setOwnerId(request.getBody().getJobOp().getData().getOwnerId());
+			jb.setJobId("listcourses");
+			jb.setStatus(JobCode.JOBRECEIVED);
+			jb.setOptions(main.build());
+			
+			JobOperation.Builder jo = JobOperation.newBuilder();
+			jo.setData(jb.build());
+			jo.setAction(JobAction.REMOVEJOB);
+			
+			pb.setJobOp(jo.build());
+			rb = Request.newBuilder();
 
-		Request.Builder rb = Request.newBuilder();
+			// metadata
+			rb.setHeader(ResourceUtil.buildHeaderFrom(request.getHeader(),
+					PokeStatus.SUCCESS, "Fetch List Of Courses"));
 
-		// metadata
-		rb.setHeader(ResourceUtil.buildHeaderFrom(request.getHeader(),
-				PokeStatus.SUCCESS, null));
+			rb.setBody(pb.build());
+				
+		}
+		else if(request.getBody().getJobOp().getData().getNameSpace().equals("coursedescription")){
+			
+			logger.info("coursedescription: " + request.getBody().getJobOp().getData().getOptions().getName()
+					+ " " + request.getBody().getJobOp().getData().getOptions().getValue());
+			String desc = "";
+			if (request.getBody().getJobOp().getData().getOptions().getValue().equals("CMPE277"))
+			{
+				desc = "Architectures, technologies, and programming concepts for developing smartphone applications. " +
+						"Covers current smartphone/tablet OSs, application development, and deployment environments. " +
+						"Prerequisites: Classified graduate standing or instructor consent";
+			}
+			
 
-		// payload
+			Payload.Builder pb = Payload.newBuilder();
+			
+			NameValueSet.Builder nb1 = NameValueSet.newBuilder();
+			nb1.setName("coursedescription");
+			nb1.setValue(desc);
+			nb1.setNodeType(NodeType.VALUE);
+			
+			JobDesc.Builder jb = JobDesc.newBuilder();
+			jb.setNameSpace("listcourses");
+			jb.setOwnerId(request.getBody().getJobOp().getData().getOwnerId());
+			jb.setJobId("listcourses");
+			jb.setStatus(JobCode.JOBRECEIVED);
+			jb.setOptions(nb1.build());
+			
+			JobOperation.Builder jo = JobOperation.newBuilder();
+			jo.setData(jb.build());
+			jo.setAction(JobAction.REMOVEJOB);
+			
+			pb.setJobOp(jo.build());
+			rb = Request.newBuilder();
 
-		Payload.Builder pb = Payload.newBuilder();
-		SignUp.Builder sb = SignUp.newBuilder();
-		sb.setEmail(request.getBody().getSignUp().getEmail());
-		sb.setPassword(request.getBody().getSignUp().getPassword());
-		sb.setFname(request.getBody().getSignUp().getFname());
-		sb.setLname(request.getBody().getSignUp().getLname());
+			// metadata
+			rb.setHeader(ResourceUtil.buildHeaderFrom(request.getHeader(),
+					PokeStatus.SUCCESS, "Fetch desc Of Courses"));
 
-		pb.setSignUp(sb.build());
-		rb.setBody(pb.build());
+			rb.setBody(pb.build());
+
+			
+		}else if(request.getBody().getJobOp().getData().getNameSpace().equals("signup")){
+   		 	logger.info("signup: " + request.getBody().getJobOp().getData().getOptions().getName()
+                	+ " " + request.getBody().getJobOp().getData().getOptions().getValue());
+        String desc = "";
+
+        
+        String fname ="" , lname = "", email = "", pwd = "";
+        if (request.getBody().getJobOp().getData().getOptions().getNode(0).getName().equals("fname"))
+        	fname =  request.getBody().getJobOp().getData().getOptions().getNode(0).getValue();
+        if (request.getBody().getJobOp().getData().getOptions().getNode(0).getName().equals("lname"))
+        	lname = request.getBody().getJobOp().getData().getOptions().getNode(1).getValue();
+        if (request.getBody().getJobOp().getData().getOptions().getNode(0).getName().equals("email"))
+            email = request.getBody().getJobOp().getData().getOptions().getNode(2).getValue();
+        if (request.getBody().getJobOp().getData().getOptions().getNode(0).getName().equals("password"))
+            pwd = request.getBody().getJobOp().getData().getOptions().getNode(3).getValue();
+   	 
+    	logger.info(fname + "  " + lname + " " + email + " " + pwd);
+
+   	 
+    	Payload.Builder pb = Payload.newBuilder();
+       	 
+        	NameValueSet.Builder nb1 = NameValueSet.newBuilder();
+        	nb1.setName("signup");
+        	nb1.setValue("signup successfull");
+        	nb1.setNodeType(NodeType.VALUE);
+       	 
+        	JobDesc.Builder jb = JobDesc.newBuilder();
+        	jb.setNameSpace("signup");
+        	jb.setOwnerId(request.getBody().getJobOp().getData().getOwnerId());
+        	jb.setJobId("signup");
+        	jb.setStatus(JobCode.JOBRECEIVED);
+        	jb.setOptions(nb1.build());
+       	 
+        	JobOperation.Builder jo = JobOperation.newBuilder();
+        	jo.setData(jb.build());
+        	jo.setAction(JobAction.REMOVEJOB);
+       	 
+        	pb.setJobOp(jo.build());
+        	rb = Request.newBuilder();
+
+        	// metadata
+        	rb.setHeader(ResourceUtil.buildHeaderFrom(request.getHeader(),
+                	PokeStatus.SUCCESS, "signup done"));
+
+        	rb.setBody(pb.build());    	 
+    }
+		else if(request.getBody().getJobOp().getData().getNameSpace().equals("signin")){
+   		 	logger.info("signin: " + request.getBody().getJobOp().getData().getOptions().getName()
+                	+ " " + request.getBody().getJobOp().getData().getOptions().getValue());
+                
+        String email = "", pwd = "";
+        if (request.getBody().getJobOp().getData().getOptions().getNode(0).getName().equals("email"))
+            email = request.getBody().getJobOp().getData().getOptions().getNode(2).getValue();
+        if (request.getBody().getJobOp().getData().getOptions().getNode(0).getName().equals("password"))
+            pwd = request.getBody().getJobOp().getData().getOptions().getNode(3).getValue();
+   	 
+    	logger.info(email + " " + pwd);
+
+   	 
+    	Payload.Builder pb = Payload.newBuilder();
+       	 
+        	NameValueSet.Builder nb1 = NameValueSet.newBuilder();
+        	nb1.setName("signin");
+        	nb1.setValue("signin successfull");
+        	nb1.setNodeType(NodeType.VALUE);
+       	 
+        	JobDesc.Builder jb = JobDesc.newBuilder();
+        	jb.setNameSpace("signin");
+        	jb.setOwnerId(request.getBody().getJobOp().getData().getOwnerId());
+        	jb.setJobId("signin");
+        	jb.setStatus(JobCode.JOBRECEIVED);
+        	jb.setOptions(nb1.build());
+       	 
+        	JobOperation.Builder jo = JobOperation.newBuilder();
+        	jo.setData(jb.build());
+        	jo.setAction(JobAction.REMOVEJOB);
+       	 
+        	pb.setJobOp(jo.build());
+        	rb = Request.newBuilder();
+
+        	// metadata
+        	rb.setHeader(ResourceUtil.buildHeaderFrom(request.getHeader(),
+                	PokeStatus.SUCCESS, "signin done"));
+
+        	rb.setBody(pb.build());    	 
+    }	
+
+
 		Request reply = rb.build();
-
 		return reply;
-
 	}
 
 }
